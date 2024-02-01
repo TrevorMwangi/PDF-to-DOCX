b3d7bbd5e6ec66e762e77ff36512a082a8651089
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-02-01 23:30:07 +0300

    tests

--- pdf_to_docx.py
+++ pdf_to_docx.py
@@ -1,8 +1,8 @@
 # pip install pdf2docx
 
 from pdf2docx import Converter
-pdf_file = '2023j.pdf'
-docx_file = '2023j.docx'
+pdf_file = 'Curriculum Vitae.pdf'
+docx_file = 'CV.docx'
 
 cv = Converter(pdf_file)
 cv.convert(docx_file)
